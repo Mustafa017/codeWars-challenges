155d576369989ca81220ab5be9bd347cf218df98
Mustafa Khamisi <mustafangure95@gmail.com>
2019-06-23 10:03:07 +0300

    fix: Optimize code to run shorter execution time

--- PrimeNumber/isPrime.js
+++ PrimeNumber/isPrime.js
@@ -1,10 +1,12 @@
 function isPrime(num) {
-  if (num <= 1) {
+  if (num < 2) {
     return false;
-  } else if (num === 2) {
+  } else if (num <= 3) {
     return true;
+  } else if (num % 2 === 0 || num % 3 === 0) {
+    return false;
   } else {
-    for (let i = 2; i < num; i++) {
+    for (let i = 5; i <= parseInt(Math.sqrt(num)); i += 2) {
       if (num % i === 0) {
         return false;
       }
@@ -12,4 +14,5 @@ function isPrime(num) {
     return true;
   }
 }
+
 module.exports = isPrime;
